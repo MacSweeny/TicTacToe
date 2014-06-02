24f9ce4138244e6a03a9639d0bc88fceaf129dfc
Andy Sweeny <andy.sweeny@xoom.com>
2014-06-02 08:36:43 -0700

    Initial Commit

---
+++ TicTacToe/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  TicTacToe
+//
+//  Created by Andy Sweeny on 6/2/14.
+//  Copyright (c) 2014 Andy Sweeny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/TTViewController.m
@@ -0,0 +1,29 @@
+//
+//  TTViewController.m
+//  TicTacToe
+//
+//  Created by Andy Sweeny on 6/2/14.
+//  Copyright (c) 2014 Andy Sweeny. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
